c2e5a40b3adf576b5915137288fb24eaaf1a99e5
heesu <tedlee0314@gmail.com>
2021-02-27 16:01:14 +0900

    first commit

---
+++ src/test/java/me/heesu/reactivespring/ReactivespringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.heesu.reactivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
